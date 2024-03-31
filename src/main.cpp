72ca55d237f3fa6818cc39c9a4f1d04ca0974739
Oopsan204 <22010740@sat.phenikaa-uni.edu.vn>
2024-03-31 15:48:08 +0700

    no message

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+/**
+ * mac id : E8:68:E7:09:1A:CC
+ * esp-now with esp 32
+ * mode: reaceive data from mutiple boards (many-to-one)
+ * note slave read sensor co, sensor sound
+ * note 1
+ * authors:oopsan
+*/
+
